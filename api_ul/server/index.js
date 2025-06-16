3cfefd49157924dd016ef0839305073a83f049c5
phoenix079 <mayukhbiswas003@gmail.com>
2025-06-16 14:02:00 +0530

    gandu

--- api_ul/server/index.js
+++ api_ul/server/index.js
@@ -16,7 +16,7 @@ const { error } = require("console");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 // Connect to the database
 
 // Security middleware
@@ -33,22 +33,9 @@ app.use(limiter);
 app.use(morgan("dev"));
 
 // Enable CORS
-const allowedOrigins = [
-  "https://final-year-project-p013.onrender.com",
-  "http://localhost:5000",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
   })
 );
 
